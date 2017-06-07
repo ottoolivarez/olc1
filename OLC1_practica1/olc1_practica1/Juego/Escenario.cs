44cbd4430bf900609dae734840425d4bc198faa1
ottoolivarez <oroc2007@gmail.com>
2017-06-07 10:57:29 -0600

    Inicio
    carga de inicio

---
+++ OLC1_practica1/olc1_practica1/Juego/Escenario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLC1_practica1.Juego
+{
+    class Escenario: System.Windows.Forms.Panel
+    {
+        Escenario() {
+            this.BackgroundImage;
+        }
+    }
+}
